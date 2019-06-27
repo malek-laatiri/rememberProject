3636f267661d8217f1a3ce1e9b0a3ecf514811db
malek laatiri <malek.laatiri73@gmail.com>
2019-06-27 16:09:10 +0100

    third commit

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\TaskRepository;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
  * Class AdminController
@@ -13,8 +14,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class AdminController extends AbstractController
 {
 
-    public function admin(UserRepository $repository)
+    public function admin(UserRepository $repository,TranslatorInterface $translator)
     {
+        $translated = $translator->trans('title');
+        $translated = $translator->trans('description');
+        $translated = $translator->trans('date');
+        $translated = $translator->trans('start hour');
+        $translated = $translator->trans('end hour');
+        $translated = $translator->trans('created');
+        $translated = $translator->trans('action');
+
+
         $users = $repository->findAll();
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
